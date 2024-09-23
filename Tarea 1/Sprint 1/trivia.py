f0e3da6f3e57c407fd24e4f0229045fd373f4998
Lerzundi <juan.lerzundi.r@uni.pe>
2024-09-23 00:54:07 -0500

    Agregar configuración básica de la API
    Agregados archivos Docker, requirements, test_trivia.py y trivia.py

---
+++ Tarea 1/Sprint 1/trivia.py
@@ -0,0 +1,24 @@
+class Question:
+    def __init__(self, description, options, correct_answer):
+        self.description = description
+        self.options = options
+        self.correct_answer = correct_answer
+
+    def is_correct(self, answer):
+        return self.correct_answer == answer
+
+
+class Quiz:
+    def __init__(self):
+        self.questions = []
+        self.current_question_index = 0
+
+    def add_question(self, question):
+        self.questions.append(question)
+
+    def get_next_question(self):
+        if self.current_question_index < len(self.questions):
+            question = self.questions[self.current_question_index]
+            self.current_question_index += 1
+            return question
+        return None
