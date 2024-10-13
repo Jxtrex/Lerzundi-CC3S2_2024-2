377dd2a7f02e64dfc32888e44f1122e5bb3504a9
Lerzundi <juan.lerzundi.r@uni.pe>
2024-10-12 21:00:44 -0500

    Agregar más tests, incluido el test idempotente
    Agregados más test y lógica de clase hasta antes de 'More Types for More Hints en el libro'

--- SANDBOX/cosmic-python/model.py
+++ SANDBOX/cosmic-python/model.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Optional, Set
+
 
 # OrderLine is an immutable dataclass with no behavior.
 @dataclass(frozen=True)
@@ -15,10 +16,24 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
-        
-    def can_allocate(self,line: OrderLine) -> bool:
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
